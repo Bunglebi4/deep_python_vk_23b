fe096e82b9f1fc4b7cd3a71aed33f2b30f7910b9
grbunkevich <bunglebi4@mail.ru>
2023-10-01 15:22:13 +0300

    add tests for json_parser

--- 02/json_parser.py
+++ 02/json_parser.py
@@ -8,3 +8,12 @@ def parse_json(json_str, keyword_callback, required_fields=None, keywords=None):
             for el in keywords:
                 if el in json_doc.get(key):
                     keyword_callback(el)
+
+
+def keyword1_callback(el):
+    print(f"i did it for {el}")
+
+
+with open("test_files_for_json_parser/good_input.json", encoding='utf-8') as fl:
+    json_str = fl.read()
+parse_json(json_str, keyword1_callback, required_fields=["name"], keywords=["Иполлит"])
---
+++ 02/json_parser_tests.py
@@ -0,0 +1,48 @@
+import unittest
+from json import JSONDecodeError
+from unittest.mock import patch, MagicMock
+from json_parser import parse_json
+
+
+class TestJsonParser(unittest.TestCase):
+    def test_parsing(self):
+        keyword_callback = MagicMock()
+        parse_json('{"key1": "Word1 word2", "key2": "word2 word3"}', keyword_callback, required_fields=["key1"],
+                   keywords=["word2"])
+        self.assertEqual(keyword_callback.call_count, 1)
+
+    def test_parsing_with_no_callback(self):
+        keyword_callback = MagicMock()
+        parse_json('{"key1": "Word1", "key2": "word2 word3"}', keyword_callback, required_fields=["key1"],
+                   keywords=["word2"])
+        self.assertEqual(keyword_callback.call_count, 0)
+
+    def test_parsing_with_no_keys(self):
+        keyword_callback = MagicMock()
+        parse_json('{"key1": "Word1", "key2": "word2 word3"}', keyword_callback, required_fields=["key8"],
+                   keywords=["word2"])
+        self.assertEqual(keyword_callback.call_count, 0)
+
+    def test_parsing_with_bad_input(self):
+        keyword_callback = MagicMock()
+        with self.assertRaises(JSONDecodeError):
+            parse_json('{1: "Word1", "key2": "word2 word3"}', keyword_callback, required_fields=["key8"],
+                       keywords=["word2"])
+
+    def test_parsing_with_no_fields(self):
+        keyword_callback = MagicMock()
+        parse_json('{"key1": "Word1", "key2": "word2 word3"}', keyword_callback, required_fields=["key8"],
+                   keywords=["word100"])
+        self.assertEqual(keyword_callback.call_count, 0)
+
+    def test_parsing_with_empty_args(self):
+        keyword_callback = MagicMock()
+        parse_json('{"key1": "Word1", "key2": "word2 word3"}', keyword_callback, required_fields=[],
+                   keywords=[])
+        self.assertEqual(keyword_callback.call_count, 0)
+
+    def test_parsing_with_two_words_in_fields(self):
+        keyword_callback = MagicMock()
+        parse_json('{"key1": "Word1", "key2": "word2 word3"}', keyword_callback, required_fields=[],
+                   keywords=[])
+        self.assertEqual(keyword_callback.call_count, 0)
