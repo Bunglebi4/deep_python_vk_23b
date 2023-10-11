085643afaed0511a97c013330854709b52adf057
grbunkevich <bunglebi4@mail.ru>
2023-10-12 01:21:27 +0300

    fixes

--- 03/custom_list.py
+++ 03/custom_list.py
@@ -1,63 +1,73 @@
 class CustomList(list):
+
     def __add__(self, other):
-        if isinstance(other, CustomList) or isinstance(other, list):
-            result = CustomList()
-            if not isinstance(other, CustomList):
-                other = CustomList(other)
-            max_len = max(len(self), len(other))
-            for i in range(max(len(self), len(other))):
-                result_elem = self[i] + other[i] if \
-                    i <= min(len(self), len(other)) - 1 else (
-                    other[i] if max_len == len(other) else self[i])
-                result.append(result_elem)
-            return CustomList(result)
-        raise TypeError(f"operation '+'"
-                        f" unsupported for {type(other)}")
+        if not (isinstance(other, CustomList) or isinstance(other, list)):
+            raise TypeError(f"operation '+'"
+                            f" unsupported for {type(other)}")
+        result = CustomList()
+        if not isinstance(other, CustomList):
+            other = CustomList(other)
+        max_len = max(len(self), len(other))
+        for i in range(max(len(self), len(other))):
+            result_elem = self[i] + other[i] if \
+                i <= min(len(self), len(other)) - 1 else (
+                other[i] if max_len == len(other) else self[i])
+            result.append(result_elem)
+        return CustomList(result)
 
     def __radd__(self, other):
-        if isinstance(other, CustomList) or isinstance(other, list):
-            result = CustomList()
-            if not isinstance(other, CustomList):
-                other = CustomList(other)
-            max_len = max(len(self), len(other))
-            for i in range(max(len(self), len(other))):
-                result_elem = other[i] + self[i] if \
-                    i <= min(len(self), len(other)) - 1 else (
-                    other[i] if max_len == len(other) else self[i])
-                result.append(result_elem)
-            return CustomList(result)
-        raise TypeError(f"operation '+'"
-                        f" unsupported for {type(other)}")
+        if not (isinstance(other, CustomList) or isinstance(other, list)):
+            raise TypeError(f"operation '+'"
+                            f" unsupported for {type(other)}")
+        result = CustomList()
+        if not isinstance(other, CustomList):
+            other = CustomList(other)
+        max_len = max(len(self), len(other))
+        for i in range(max(len(self), len(other))):
+            result_elem = other[i] + self[i] if \
+                i <= min(len(self), len(other)) - 1 else (
+                other[i] if max_len == len(other) else self[i])
+            result.append(result_elem)
+        return CustomList(result)
 
     def __sub__(self, other):
-        if isinstance(other, CustomList) or isinstance(other, list):
-            result = CustomList()
-            if not isinstance(other, CustomList):
-                other = CustomList(other)
-            max_len = max(len(self), len(other))
-            for i in range(max(len(self), len(other))):
-                result_elem = self[i] - other[i] if \
-                    i <= min(len(self), len(other)) - 1 else (
-                    -other[i] if max_len == len(other) else self[i])
-                result.append(result_elem)
-            return result
-        raise TypeError(f"operation '-'"
-                        f" unsupported for {type(other)}")
+        if not (isinstance(other, CustomList) or isinstance(other, list)):
+            raise TypeError(f"operation '-'"
+                            f" unsupported for {type(other)}")
+        result = CustomList()
+        if not isinstance(other, CustomList):
+            other = CustomList(other)
+        max_len = max(len(self), len(other))
+        for i in range(max(len(self), len(other))):
+            result_elem = self[i] - other[i] if \
+                i <= min(len(self), len(other)) - 1 else (
+                -other[i] if max_len == len(other) else self[i])
+            result.append(result_elem)
+        return result
 
     def __rsub__(self, other):
-        if isinstance(other, CustomList) or isinstance(other, list):
-            if not isinstance(other, CustomList):
-                other = CustomList(other)
-            max_len = max(len(other), len(self))
-            result = CustomList()
-            for i in range(max(len(other), len(self))):
-                result_elem = other[i] - self[i] if \
-                    i <= min(len(other), len(self)) - 1 else (
-                    -self[i] if max_len == len(self) else other[i])
-                result.append(result_elem)
-            return result
-        raise TypeError(f"operation '-'"
-                        f" unsupported for {type(other)}")
+        if not (isinstance(other, CustomList) or isinstance(other, list)):
+            raise TypeError(f"operation '-'"
+                            f" unsupported for {type(other)}")
+        if not isinstance(other, CustomList):
+            other = CustomList(other)
+        max_len = max(len(other), len(self))
+        result = CustomList()
+        for i in range(max(len(other), len(self))):
+            result_elem = other[i] - self[i] if \
+                i <= min(len(other), len(self)) - 1 else (
+                -self[i] if max_len == len(self) else other[i])
+            result.append(result_elem)
+        return result
+
+    @staticmethod
+    def check_equality(lst1, lst2):
+        if len(lst1) != len(lst2):
+            return False
+        for i in range(len(lst1)):
+            if lst1[i] != lst2[i]:
+                return False
+        return True
 
     def __eq__(self, other):
         return sum(self) == sum(other)
@@ -69,28 +79,29 @@ class CustomList(list):
                         f" unsupported for {type(other)}")
 
     def __le__(self, other):
-        if isinstance(other, CustomList):
-            return sum(self) <= sum(other)
-        raise TypeError(f"operation '<='"
-                        f" unsupported for {type(other)}")
+        if not isinstance(other, CustomList):
+            raise TypeError(f"operation '<='"
+                            f" unsupported for {type(other)}")
+        return sum(self) <= sum(other)
+
 
     def __lt__(self, other):
-        if isinstance(other, CustomList):
-            return sum(self) < sum(other)
-        raise TypeError(f"operation '<'"
-                        f" unsupported for {type(other)}")
+        if not isinstance(other, CustomList):
+            raise TypeError(f"operation '<'"
+                            f" unsupported for {type(other)}")
+        return sum(self) < sum(other)
 
     def __ge__(self, other):
-        if isinstance(other, CustomList):
-            return sum(self) >= sum(other)
-        raise TypeError(f"operation '>='"
-                        f" unsupported for {type(other)}")
+        if not isinstance(other, CustomList):
+            raise TypeError(f"operation '>='"
+                            f" unsupported for {type(other)}")
+        return sum(self) >= sum(other)
 
     def __gt__(self, other):
-        if isinstance(other, CustomList):
-            return sum(self) > sum(other)
-        raise TypeError(f"operation '>'"
-                        f" unsupported for {type(other)}")
+        if not isinstance(other, CustomList):
+            raise TypeError(f"operation '>'"
+                            f" unsupported for {type(other)}")
+        return sum(self) > sum(other)
 
     def __str__(self):
         return f'CustomList = {list(self)}, sum = {sum(self)}'
--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -9,56 +9,56 @@ class TestCustomList(unittest.TestCase):
         obj_2 = CustomList([0, 3, 1, 3, 3])
         result = obj_1 + obj_2
         expected = CustomList([0, 6, 3, 6, 6])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_with_different_len(self):
         obj_1 = CustomList([0, 3, 2, 3, 3])
         obj_2 = CustomList([0, 3, 1, 3, 3, 0, 1, 2])
         result = obj_1 + obj_2
         expected = CustomList([0, 6, 3, 6, 6, 0, 1, 2])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_with_diff_len_v_2(self):
         obj_1 = CustomList([0, 3, 2, 3, 3, 1, 2])
         obj_2 = CustomList([0, 3, 1, 3, 3])
         result = obj_1 + obj_2
         expected = CustomList([0, 6, 3, 6, 6, 1, 2])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_with_list(self):
         obj_1 = CustomList([0, 3, 2, 3, 3])
         obj_2 = [0, 3, 1, 3, 3]
         result = obj_1 + obj_2
         expected = CustomList([0, 6, 3, 6, 6])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_with_list_with_diff_len(self):
         obj_1 = CustomList([0, 3, 2, 3, 3, 1, 2, 3])
         obj_2 = [0, 3, 1, 3, 3]
         result = obj_1 + obj_2
         expected = CustomList([0, 6, 3, 6, 6, 1, 2, 3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_with_list_with_diff_len_v2(self):
         obj_1 = CustomList([0, 3, 2, 3, 3])
         obj_2 = [0, 3, 1, 3, 3, 1, 2, 3]
         result = obj_1 + obj_2
         expected = CustomList([0, 6, 3, 6, 6, 1, 2, 3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_list_with_custom_list(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = [0, 3, 1, 3, 3]
         result = obj2 + obj1
         expected = CustomList([0, 6, 3, 6, 6])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_list_with_custom_with_diff_len(self):
         obj1 = CustomList([0, 3, 2, 3, 3, 1, 2, 3])
         obj2 = [0, 3, 1, 3, 3]
         result = obj2 + obj1
         expected = CustomList([0, 6, 3, 6, 6, 1, 2, 3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_addition_list_with_custom_with_diff_len2(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
@@ -66,70 +66,70 @@ class TestCustomList(unittest.TestCase):
         result = obj2 + obj1
         expected = CustomList([0, 6, 3, 6, 6, 1, 2, 3])
         print(type(result))
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = CustomList([0, 3, 1, 3, 3])
         result = obj1 - obj2
         expected = CustomList([0, 0, 1, 0, 0])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_with_different_len(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = CustomList([0, 3, 1, 3, 3, 0, 1, 2])
         result = obj1 - obj2
         expected = CustomList([0, 0, 1, 0, 0, 0, -1, -2])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_with_diff_len_v_2(self):
         obj1 = CustomList([0, 3, 2, 3, 3, 1, 2])
         obj2 = CustomList([0, 3, 1, 3, 3])
         result = obj1 - obj2
-        expected = CustomList([0, 0, 1, 0, 0, 0, 1, 2])
-        self.assertEqual(result, expected)
+        expected = CustomList([0, 0, 1, 0, 0, 1, 2])
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_with_list(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = [0, 3, 1, 3, 3]
         result = obj1 - obj2
         expected = CustomList([0, 0, 1, 0, 0])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_with_list_with_diff_len(self):
         obj1 = CustomList([0, 3, 2, 3, 3, 1, 2, 3])
         obj2 = [0, 3, 1, 3, 3]
         result = obj1 - obj2
         expected = CustomList([0, 0, 1, 0, 0, 1, 2, 3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_with_list_with_diff_len_v2(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = [0, 3, 1, 3, 3, 1, 2, 3]
         result = obj1 - obj2
         expected = CustomList([0, 0, 1, 0, 0, -1, -2, -3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_from_list(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = [0, 3, 1, 3, 3]
         result = obj2 - obj1
         expected = CustomList([0, 0, -1, 0, 0])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_from_list_with_diff_len(self):
         obj1 = CustomList([0, 3, 2, 3, 3, 1, 2, 3])
         obj2 = [0, 3, 1, 3, 3]
         result = obj2 - obj1
         expected = CustomList([0, 0, -1, 0, 0, -1, -2, -3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_subtraction_with_from_with_diff_len_v2(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
         obj2 = [0, 3, 1, 3, 3, 1, 2, 3]
         result = obj2 - obj1
         expected = CustomList([0, 0, -1, 0, 0, 1, 2, 3])
-        self.assertEqual(result, expected)
+        self.assertEqual(list(result), list(expected))
 
     def test_equality(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
@@ -195,7 +195,7 @@ class TestCustomList(unittest.TestCase):
         obj1 = CustomList([None])
         obj2 = CustomList([None])
         with self.assertRaises(TypeError):
-            obj1 + obj2
+            return obj1 + obj2
 
     def test_type(self):
         obj1 = CustomList([0, 3, 2, 3, 3])
@@ -209,17 +209,17 @@ class TestCustomList(unittest.TestCase):
         self.assertIsInstance(obj2 - obj1, CustomList)
 
     def test_lt(self):
-        obj1 = CustomList([0, 3, 1, 3, 3])
+        obj1 = CustomList([0, 23, -100, 3, 3])
         obj2 = CustomList([0, 3, 2, 3, 3])
         self.assertTrue(obj1 < obj2)
 
     def test_false_lt(self):
-        obj1 = CustomList([0, 3, 3, 3, 3])
+        obj1 = CustomList([0, 7, 3, 9, 3])
         obj2 = CustomList([0, 3, 2, 3, 3])
         self.assertFalse(obj1 < obj2)
 
     def test_equal_lt(self):
-        obj1 = CustomList([0, 3, 3, 3, 3])
+        obj1 = CustomList([0, 9, 0, 0, 3])
         obj2 = CustomList([0, 3, 3, 3, 3])
         self.assertFalse(obj1 < obj2)
 
@@ -243,81 +243,102 @@ class TestCustomList(unittest.TestCase):
         obj2 = CustomList([0, 3, 2, 3, 3])
         expected_obj1 = CustomList([0, 3, 3, 3, 3])
         expected_obj2 = CustomList([0, 3, 2, 3, 3])
-        obj2 + obj1
-        obj2 - obj1
-        obj1 - obj2
-        self.assertEqual(obj1, expected_obj1)
-        self.assertEqual(obj2, expected_obj2)
+        var = obj2 + obj1
+        var2 = obj2 - obj1
+        var3 = obj1 - obj2
+        self.assertEqual(list(obj1), list(expected_obj1))
+        self.assertEqual(list(obj2), list(expected_obj2))
+        return var, var2, var3
 
     def test_add_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
-            obj1 + obj2
+            var = obj1 + obj2
+            return var
 
     def test_sub_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
-            obj1 - obj2
+            var = obj1 - obj2
+            return var
 
     def test_rsub_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
-            obj2 - obj1
-
-    def test_eq_type_error(self):
-        obj1 = CustomList([0, 1, 2, 3])
-        obj2 = "sodjkfgnsodjfgn"
-        with self.assertRaises(TypeError):
-            var = obj1 == obj2
+            var = obj2 - obj1
+            return var
 
     def test_ne_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
             var = obj1 != obj2
+            return var
 
     def test_le_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
             var = obj1 <= obj2
+            return var
 
     def test_lt_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
             var = obj1 < obj2
+            return var
 
     def test_ge_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
             var = obj1 >= obj2
+            return var
 
     def test_gt_type_error(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
             var = obj1 > obj2
+            return var
 
     def test_radd(self):
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = "sodjkfgnsodjfgn"
         with self.assertRaises(TypeError):
             var = obj2 + obj1
+            return var
+
         obj1 = CustomList([0, 1, 2, 3])
         obj2 = [0, -2, 3]
         expected = CustomList([0, -1, 5, 3])
         self.assertIsInstance(obj2 + obj1, CustomList)
-        self.assertEqual(obj2 + obj1, expected)
+        self.assertEqual(list(obj2 + obj1), list(expected))
 
     def test_add_with_negative(self):
         obj1 = CustomList([-1, -2, -3, -5])
         obj2 = [-1, -1, -1]
         expected = CustomList([-2, -3, -4, -5])
         self.assertIsInstance(obj2 + obj1, CustomList)
-        self.assertEqual(obj2 + obj1, expected)
-        self.assertEqual(obj1 + obj2, expected)
+        self.assertEqual(list(obj2 + obj1), list(expected))
+        self.assertEqual(list(obj1 + obj2), list(expected))
+
+    def test_add_empty_list(self):
+        self.assertEqual(
+            list(CustomList([]) + []),
+            []
+        )
+
+    def test_sub_empty_list(self):
+        self.assertEqual(
+            list(CustomList([]) - []),
+            []
+        )
+
+    def test_empty_list_str(self):
+        obj = CustomList([])
+        self.assertEqual(obj.__str__(), 'CustomList = [], sum = 0')
