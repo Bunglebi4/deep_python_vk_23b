0612e8bab1e9846c6115b9f79bf20a06ae7f3fbc
grbunkevich <bunglebi4@mail.ru>
2023-10-18 06:43:57 +0300

    add tests for metaclass

--- 04/metaclass.py
+++ 04/metaclass.py
@@ -2,7 +2,7 @@ class CustomMeta(type):
     def __new__(cls, name, bases, dct):
         custom_dct = {}
         for attr_name, attr_value in dct.items():
-            if not (attr_name.startswith('__') or attr_name.endswith('__')):
+            if not (attr_name.startswith('__') and attr_name.endswith('__')):
                 custom_attr_name = f'custom_{attr_name}'
             else:
                 custom_attr_name = attr_name
@@ -11,64 +11,4 @@ class CustomMeta(type):
         return super().__new__(cls, name, bases, custom_dct)
 
     def __setattr__(self, name, value):
-        if name.startswith('custom_'):
-            super().__setattr__(name, value)
-        else:
-            self.__dict__[f"custom_{name}"] = value
-
-
-class CustomClass(metaclass=CustomMeta):
-    x = 50
-
-    def __init__(self, val=99):
-        self.val = val
-
-    def line(self):
-        return 100
-
-    def __str__(self):
-        return "Custom_by_metaclass"
-
-
-inst = CustomClass()
-inst.aboba = 50
-print(CustomClass.__dict__)
-assert CustomClass.custom_x == 50
-assert inst.custom_x == 50
-assert inst.custom_val == 99
-assert inst.custom_line() == 100
-assert str(inst) == "Custom_by_metaclass"
-
-try:
-    CustomClass.x
-except AttributeError:
-    pass
-
-try:
-    inst.x
-except AttributeError:
-    pass
-
-try:
-    inst.val
-except AttributeError:
-    pass
-
-try:
-    inst.line()
-except AttributeError:
-    pass
-
-try:
-    inst.yyy
-except AttributeError:
-    pass
-
-inst.dynamic = "added later"
-assert inst.custom_dynamic == "added later"
-
-# Попытка обращения к атрибутам и методам без префикса "custom_"
-try:
-    inst.dynamic
-except AttributeError:
-    pass
+        self.__dict__[f"custom_{name}"] = value
---
+++ 04/test_metaclass.py
@@ -0,0 +1,105 @@
+import unittest
+from metaclass import CustomMeta
+
+
+class TestCustomMeta(unittest.TestCase):
+    class CustomClass(metaclass=CustomMeta):
+        x = 50
+
+        def __init__(self, val=99):
+            self.val = val
+
+        def line(self):
+            return 100
+
+        def __str__(self):
+            return "Custom_by_metaclass"
+
+    def test_customizing_fields(self):
+        inst = self.CustomClass()
+        self.assertEqual(inst.custom_x, 50)
+        self.assertEqual(inst.custom_val, 99)
+        self.assertEqual(inst.custom_line(), 100)
+        self.assertEqual(self.CustomClass.custom_x, 50)
+        self.assertEqual(str(inst), "Custom_by_metaclass")
+
+    def test_original_fields(self):
+        inst = self.CustomClass()
+        with self.assertRaises(AttributeError):
+            return inst.x
+        with self.assertRaises(AttributeError):
+            return inst.val
+        with self.assertRaises(AttributeError):
+            return inst.line()
+        with self.assertRaises(AttributeError):
+            return inst.yyy
+        with self.assertRaises(AttributeError):
+            return self.CustomClass.x
+
+    def test_dynamic_attrs(self):
+        inst = self.CustomClass()
+        inst.dynamic = "added later"
+        self.assertEqual(inst.custom_dynamic, "added later")
+        self.assertTrue("custom_dynamic" in inst.__dict__ and
+                        "dynamic" not in inst.__dict__)
+
+    def test_set_custom_attribute_in_constructor(self):
+        obj = self.CustomClass(val=42)
+        self.assertEqual(obj.custom_val, 42)
+
+    def test_custom_names(self):
+        class CustomClass(metaclass=CustomMeta):
+            def custom_method(self):
+                return "Custom Method"
+
+        self.assertTrue(hasattr(CustomClass, 'custom_custom_method'))
+
+    def test_changing_attrs(self):
+        obj = self.CustomClass(42)
+        obj.attribute = 'test'
+        original_value = obj.custom_attribute
+        obj.custom_attribute = 'new_test'
+        self.assertEqual(original_value, obj.custom_attribute)
+
+    def test_private_attrs(self):
+        class Biba(metaclass=CustomMeta):
+            def __init__(self, value):
+                self._value = value
+
+        self.assertTrue("custom__value" in Biba(12).__dict__)
+
+    def test_custom_method(self):
+        class Buba(metaclass=CustomMeta):
+            def real_method(self):
+                print("i'll be custom")
+
+            def second_useless_method(self):
+                print('dont look at me im useless')
+
+        self.assertTrue(hasattr(Buba(), "custom_real_method"))
+
+    def test_private_custom_method(self):
+        class Buba(metaclass=CustomMeta):
+            def _real_method(self):
+                print("i'll be custom")
+
+            def second_useless_method(self):
+                print('dont look at me im useless')
+
+        self.assertTrue(hasattr(Buba(), "custom__real_method"))
+
+    def test_protected_custom_method(self):
+        class Buba(metaclass=CustomMeta):
+            def __real_method(self):
+                print("i'll be custom")
+
+            def second_useless_method(self):
+                print('dont look at me im useless')
+
+        self.assertTrue(hasattr(Buba(), "custom__Buba__real_method"))
+
+    def test_protected_attr(self):
+        class Buba(metaclass=CustomMeta):
+            def __init__(self, x):
+                self.__x = x
+        self.assertTrue(hasattr(Buba(12), "custom__Buba__x"))
