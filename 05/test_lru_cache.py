5a038a660f05a6fbad40861cfee7747a4f26c203
grbunkevich <bunglebi4@mail.ru>
2023-10-26 18:04:31 +0300

    add tests

--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -49,3 +49,27 @@ class TestLruCache(unittest.TestCase):
         self.assertEqual(factorial_with_cache(4, cache), 24)
         self.assertEqual(factorial_with_cache(5, cache), 120)
         self.assertEqual(factorial_with_cache(5, cache), 120)
+
+    def test_cache_eviction(self):
+        cache = LRUCache(3)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k3", "val3")
+        cache.set("k4", "val4")
+        self.assertIs(cache.get("k1"), None)
+        self.assertEqual(cache.get("k2"), "val2")
+        self.assertEqual(cache.get("k3"), "val3")
+        self.assertEqual(cache.get("k4"), "val4")
+
+    def test_cache_with_duplicate_keys(self):
+        cache = LRUCache(3)
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k2", "val2-updated")
+        self.assertEqual(cache.get("k1"), "val1")
+        self.assertEqual(cache.get("k2"), "val2-updated")
+
+    def test_cache_with_zero_capacity(self):
+        cache = LRUCache(0)
+        with self.assertRaises(IndexError):
+           return cache.set("k1", "val1")
