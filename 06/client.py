7a7bce3d17d1f24e48d32b0f44a597077b43507b
grbunkevich <bunglebi4@mail.ru>
2023-11-02 17:48:16 +0300

    add hw 6

---
+++ 06/client.py
@@ -0,0 +1,39 @@
+import argparse
+import socket
+import threading
+
+
+def send_urls(urls, socket_server):
+    data = "\n".join(urls)
+    socket_server.send(data.encode('utf-8'))
+    response = socket_server.recv(4096)
+    result = response.decode('utf-8')
+    print(result)
+    socket_server.close()
+
+
+def main_func(filename, num_threads):
+    print("AAAa")
+    num_threads = args.t if args.t is not None else 1
+    with open(filename, encoding='utf-8') as file:
+        urls = file.read().split()
+    chunk_size = len(urls) // num_threads
+    threads = []
+    for i in range(0, len(urls), 1):
+        chunk = urls[i:i + chunk_size]
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 8888))
+        print("Connected to server")
+        thread = threading.Thread(target=send_urls, args=(chunk, client_socket))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+    print("all doone ")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file_path', type=str, help='File with URLs')
+    parser.add_argument('-t', type=int, help='threads')
+    args = parser.parse_args()
+    main_func(args.file_path, args.t)
---
+++ 06/server.py
@@ -0,0 +1,75 @@
+import socket
+import json
+import threading
+from collections import Counter
+from queue import Queue
+from urllib.request import urlopen
+import bs4
+
+
+def handle_client(client_socket):
+    data = client_socket.recv(1024)
+    url = data.decode('utf-8')
+
+    response = crawl_url(url)
+
+    client_socket.send(json.dumps(response).encode('utf-8'))
+    client_socket.close()
+
+
+def crawl_url(url):
+    with urlopen(url) as page_info:
+        page = page_info.read().decode('utf-8')
+
+    soup = bs4.BeautifulSoup(page, "html.parser")
+    text = soup.get_text()
+    words = text.split()
+    word_count = Counter(words)
+    top_k_words = dict(sorted(word_count.items(), key=lambda x: x[1], reverse=True)[:K])
+    print(top_k_words)
+    return top_k_words
+
+
+def worker():
+    while True:
+        url = url_queue.get()
+        if url is None:
+            break
+        response = crawl_url(url)
+        result_queue.put(response)
+        url_queue.task_done()
+
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 8888))
+    server.listen(5)
+
+    print("Сервер готов к приему запросов.")
+
+    for _ in range(workers):
+        trhead = threading.Thread(target=worker)
+        trhead.start()
+
+    while True:
+        client_socket, addr = server.accept()
+        print(f"Принято соединение от {addr}")
+        data = client_socket.recv(1024)
+        url = data.decode('utf-8')
+        url_queue.put(url)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Сервер для обкачки URL")
+    parser.add_argument("-w", type=int, help="Количество воркеров", required=True)
+    parser.add_argument("-k", type=int, help="Топ-K частых слов", required=True)
+    args = parser.parse_args()
+
+    K = args.k
+    workers = args.w
+
+    url_queue = Queue()
+    result_queue = Queue()
+    main()
