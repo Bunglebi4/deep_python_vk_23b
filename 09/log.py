8ccb0563ae51d79acc5d21a1030a9d5a96a0b9dd
grbunkevich <bunglebi4@mail.ru>
2023-11-23 17:18:08 +0300

    add hw9

---
+++ 09/log.py
@@ -0,0 +1,94 @@
+import argparse
+import logging
+
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.order = []
+
+    def get(self, key):
+        if key not in self.cache:
+            logging.debug(f"Key '{key}' not found in cache")
+            return self.cache.get(key)
+        self.order.remove(key)
+        self.order.append(key)
+        logging.debug(f"Key '{key}' found in cache. Value: {self.cache[key]}")
+        return self.cache[key]
+
+    def set(self, key, value) -> None:
+        if key in self.cache:
+            logging.debug(f"Key '{key}' already exists. Updating value.")
+            self.order.remove(key)
+            del self.cache[key]
+        elif len(self.cache) >= self.capacity:
+            evicted_key = self.order.pop(0)
+            logging.debug(f"Evicting key '{evicted_key}' to make space.")
+            del self.cache[evicted_key]
+        logging.debug(f"Setting key '{key}' with value '{value}' in cache.")
+        self.cache[key] = value
+        self.order.append(key)
+
+
+def setup_logging(log_to_stdout, custom_filter):
+    formatter_for_stream = logging.Formatter(
+        "[%(levelname)s] [%(asctime)s] [%(name)s] [%(filename)s:%(lineno)d] - %(message)s"
+    )
+    formatter_for_file = logging.Formatter(
+        "[%(levelname)s] [%(asctime)s] [%(module)s:%(lineno)d] - %(message)s"
+    )
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG if log_to_stdout else logging.INFO)
+    stream_handler.setFormatter(formatter_for_stream)
+
+    file_handler = logging.FileHandler("cache.log", mode="w", encoding="utf-8")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter_for_file)
+
+    logger.addHandler(file_handler)
+
+    if log_to_stdout:
+        logger.addHandler(stream_handler)
+
+    if custom_filter:
+        class CustomFilter(logging.Filter):
+            def filter(self, record):
+                return len(record.msg.split()) % 2 != 0
+
+        logger.addFilter(CustomFilter())
+
+    logger.info("Logging setup completed")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='LRUCache logging example')
+    parser.add_argument('-s', action='store_true', help='Log to stdout')
+    parser.add_argument('-f', action='store_true', help='Apply custom filter')
+
+    args = parser.parse_args()
+
+    cache = LRUCache(3)
+
+    setup_logging(args.s, args.f)
+
+    cache.set(1, 'one')
+    cache.get(1)
+    cache.get(2)
+    cache.set(3, 'three')
+    cache.set(4, 'four')
+
+    logging.debug("Debug message")
+
+    cache.get(1)
+    cache.get(3)
+    cache.set(5, 'five')
+
+    logging.debug("Additional debug message")
